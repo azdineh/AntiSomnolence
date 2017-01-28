36380f2164745560e0e33523bd9b6d4a33275981
HILALI Azedine <azdineh@gmail.com>
2017-01-28 17:04:18 +0000

    Add background-mode cordova plugin

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,15 +1,65 @@
 angular.module('starter.controllers', [])
-.controller('HomeCtrl', function($scope) {
-  // With the new view caching in Ionic, Controllers are only called
-  // when they are recreated or on app start, instead of every page change.
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
-  //$scope.chat = Chats.get($stateParams.chatId);
-})
-.controller('MbreakerCtrl',function($scope){})
-.controller('CarCtrl',function($scope){})
-.controller('UserCtrl',function($scope){})
-.controller('SettingsCtrl',function($scope){});
+  .controller('HomeCtrl', function ($scope) {
+    // With the new view caching in Ionic, Controllers are only called
+    // when they are recreated or on app start, instead of every page change.
+    // To listen for when this page is active (for example, to refresh data),
+    // listen for the $ionicView.enter event:
+    //
+    //$scope.$on('$ionicView.enter', function(e) {
+    //});
+    //$scope.chat = Chats.get($stateParams.chatId);
+  })
+  .controller('MbreakerCtrl', function ($scope, $rootScope, $interval,smbreaker) {
+
+    $scope.slidesOptions = {
+      loop: false,
+      speed: 250,
+      pagination: false
+    };
+
+    $rootScope.waitedDelay = 1*1000*60; //tow minuts
+    $rootScope.frequencePulse = 0.5*1000*60; // time between excitations
+    $rootScope.enableAdvice = false;
+    $scope.currentSS = "";
+    $scope.timerSecond = 59;
+    $scope.timerMinut = $rootScope.waitedDelay / 60000;
+
+    //var t=0;
+    /*var count = $rootScope.waitedDelay / 1000;
+    $interval(function () {
+      if ($scope.timerSecond === 0) {
+        $scope.timerMinut=$scope.timerMinut-1;
+        $scope.timerSecond = 59;
+      }
+      else {
+        $scope.timerSecond = Math.abs($scope.timerSecond - 1);
+      }
+
+    }, 1000, count);*/
+    $scope.d=Date.now();
+
+    var bootfunc=$interval(function(){
+      $scope.d0=Date.now();
+    },1000);
+
+    $scope.$watch('d0',function(d1){
+      console.log("now :"+d1);
+      if(Math.abs(d1-(Math.abs($scope.d)+Math.abs($rootScope.waitedDelay)))<500){
+        console.log("Start breaking now..");
+        smbreaker.startSoundMonotonyBreaking($rootScope.waitedDelay,$rootScope.frequencePulse,$rootScope.enableAdvice,$scope);
+        $interval.cancel(bootfunc);
+      }
+    },true);
+
+    $scope.$on('$destroy', function () {
+      smbreaker.stopBreaking();
+    });
+
+
+
+
+
+  })
+  .controller('CarCtrl', function ($scope) { })
+  .controller('UserCtrl', function ($scope) { })
+  .controller('SettingsCtrl', function ($scope) { });
--- www/js/services.js
+++ www/js/services.js
@@ -1,50 +1,129 @@
 angular.module('starter.services', [])
 
-.factory('Chats', function() {
-  // Might use a resource here that returns a JSON array
+  .factory('Chats', function () {
+    var x = 0;
+    return {
+      X: x
+    };
+  })
+  // Implentation of sound monotony breaker
+  .factory('smbreaker', function ($interval, $timeout, $http, $q, $cordovaNativeAudio) {
 
-  // Some fake testing data
-  var chats = [{
-    id: 0,
-    name: 'Ben Sparrow',
-    lastText: 'You on your way?',
-    face: 'img/ben.png'
-  }, {
-    id: 1,
-    name: 'Max Lynx',
-    lastText: 'Hey, it\'s me',
-    face: 'img/max.png'
-  }, {
-    id: 2,
-    name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
-    face: 'img/adam.jpg'
-  }, {
-    id: 3,
-    name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
-    face: 'img/perry.png'
-  }, {
-    id: 4,
-    name: 'Mike Harrington',
-    lastText: 'This is wicked good ice cream.',
-    face: 'img/mike.png'
-  }];
+    var vm = {};
+    /**
+     * @description SS for ShortSound is an json object like {id:'zedge',name:'zedge.mp3'} 
+     */
+    var SS = function (id, name, type) {
+      var that = {};
+      that.id = id;
+      that.name = name;
+      that.type = type;
 
-  return {
-    all: function() {
-      return chats;
-    },
-    remove: function(chat) {
-      chats.splice(chats.indexOf(chat), 1);
-    },
-    get: function(chatId) {
-      for (var i = 0; i < chats.length; i++) {
-        if (chats[i].id === parseInt(chatId)) {
-          return chats[i];
-        }
+      return that;
+    };
+
+    var SSarray = [];
+
+    /**
+     * @param url like sound/shortsounds.json
+     */
+    var fillSSarrayfromJsonFile = function (url) {
+      var q = $q.defer();
+      var urlsuffix = "";
+
+      if (ionic.Platform.isAndroid()) {
+        urlsuffix = "/android_asset/www/";
       }
-      return null;
-    }
-  };
-});
+
+      $http.get(urlsuffix + url)
+        .then(function (res) {
+          q.resolve(res.data.shortsounds);
+        }, function (err) {
+          console.log("Error while getting resources..");
+          q.reject(err);
+        });
+
+      return q.promise;
+    };
+
+    /**
+     * @param SSobject 
+     */
+    var playSS = function (SSobject, scope) {
+      var path = "sound/SS/";
+      if (SSobject.type == 'ssa') { path = 'sound/SSAdvice/'; }
+
+      $cordovaNativeAudio
+        .preloadComplex(SSobject.id, path + SSobject.name + '.mp3', 1, 1)
+        .then(function (msg) {
+
+          if (SSobject.type == 'SS')
+            console.log('Short sound is prelaoded successfully..');
+          else
+            console.log('Short sound advice is prelaoded successfully..');
+
+          //$cordovaNativeAudio.loop(SSobject.id);
+          $cordovaNativeAudio.play(SSobject.id, function () {
+            console.log("End playing of : " + SSobject.name);
+            $cordovaNativeAudio.unload(SSobject.id);
+          }, function () { }, function () { });
+
+        }, function (error) {
+          console.log('Error ' + error + ' while preloading this sound ' + SSobject.name + '.mp3');
+        });
+    };
+
+    var i = 0;
+    var startingPromise;
+    /**
+     * @param time_between_excitation number of milliseconds
+     * @param ifadvice boolean for enable short advice sounds 
+     */
+    vm.startSoundMonotonyBreaking = function (waited_delayy, time_between_excitation, ifadvice, scope) {
+      var advice = ifadvice || false;
+      var waited_delay = waited_delayy || 120000; // tow minuts
+      var tbe = time_between_excitation || 3000; // tbe from time_between_excitation;
+
+
+
+      fillSSarrayfromJsonFile("sound/shortsounds.json")
+        .then(function (array) {
+          SSarray = array;
+          var ssCount = SSarray.length;
+          var y = 1, x = 1;
+
+          startingPromise = $interval(function () {
+
+            while (x == y || (SSarray[y].type == "ssa") && !ifadvice) {
+              y = Math.floor(Math.random() * ssCount);
+            }
+
+            console.log("excitation : y = " + y + " " + SSarray[y].name + " type : " + SSarray[y].type); i++;
+            x = y;
+
+            scope.currentSS = SSarray[y].name;
+            // play a random SS 
+            playSS(SSarray[y], scope);
+
+          }, tbe);
+
+
+
+
+
+        }, function (err) {
+
+        });
+
+    };
+
+    vm.stopBreaking = function () {
+      if (angular.isDefined(startingPromise)) {
+        $interval.cancel(startingPromise);
+        startingPromise = undefined;
+      }
+    };
+
+
+    return vm;
+  });
